7166e8f57ceeea287600345dfc6adaddb7b2d186
Sean Shin <shin.seang@gmail.com>
2022-09-30 10:33:40 -0400

    added remaining timeblocks to HTML and event listeners in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var currentDayEl = $("#currentDay");
 var tableEl = $(".table");
-var timeTable = [$("#9amInput"), $("#10amInput")];
+var timeTable = [$("#9amInput"), $("#10amInput"), $("#11amInput"), $("#12pmInput"), $("#1pmInput"), $("#2pmInput"), $("#3pmInput"), $("#4pmInput"), $("#5pmInput")];
+var clearBtnEl = $("#clearBtn")
 
 var entries = [];
 
@@ -13,7 +14,6 @@ function init() {
     for(var i=0; i<oldEntries.length; i++) {
       if(oldEntries[i]!=null) {
         entries[i]=oldEntries[i];
-        console.log(entries[i]);
       }
     }
   }
@@ -36,15 +36,47 @@ function displayOldEntries() {
 //the target property of the event object is used to check which save is clicked
 tableEl.on("click", function(event) {
   if($(event.target).attr("id")=="9amBtn") {
-    console.log("you clicked save button 9am");//debug
     entries[0] = timeTable[0].val();
-    console.log(entries[0]);
     localStorage.setItem("oldEntries", JSON.stringify(entries));
   }
   if($(event.target).attr("id")=="10amBtn") {
-    console.log("you clicked save button 10am");//debug
     entries[1] = timeTable[1].val();
-    console.log(entries[1]);
     localStorage.setItem("oldEntries", JSON.stringify(entries));
   }
+  if($(event.target).attr("id")=="11amBtn") {
+    entries[2] = timeTable[2].val();
+    localStorage.setItem("oldEntries", JSON.stringify(entries));
+  }
+  if($(event.target).attr("id")=="12pmBtn") {
+    entries[3] = timeTable[3].val();
+    localStorage.setItem("oldEntries", JSON.stringify(entries));
+  }
+  if($(event.target).attr("id")=="1pmBtn") {
+    entries[4] = timeTable[4].val();
+    localStorage.setItem("oldEntries", JSON.stringify(entries));
+  }
+  if($(event.target).attr("id")=="2pmBtn") {
+    entries[5] = timeTable[5].val();
+    localStorage.setItem("oldEntries", JSON.stringify(entries));
+  }
+  if($(event.target).attr("id")=="3pmBtn") {
+    entries[6] = timeTable[6].val();
+    localStorage.setItem("oldEntries", JSON.stringify(entries));
+  }
+  if($(event.target).attr("id")=="4pmBtn") {
+    entries[7] = timeTable[7].val();
+    localStorage.setItem("oldEntries", JSON.stringify(entries));
+  }
+  if($(event.target).attr("id")=="5pmBtn") {
+    entries[8] = timeTable[8].val();
+    localStorage.setItem("oldEntries", JSON.stringify(entries));
+  }
+})
+//Event listener for clicks on the clear button, which will clear the entries and local storage
+clearBtnEl.on("click", function() {
+  for(var i=0; i<timeTable.length; i++) {
+    timeTable[i].val("");
+    entries[i] = timeTable[i].val();
+  }
+  localStorage.setItem("oldEntries", JSON.stringify(entries));
 })
